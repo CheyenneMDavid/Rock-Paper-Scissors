4f5231cc56ef410d59f0e76fb707bdc4a1df3ca3
CheyenneMDavid <Cheyennedavid68@gmail.com>
2022-12-21 21:07:54 +0000

    Add increment score functions for user and comuter

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,52 @@
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
-
     let userChoice = ""
-
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
 
             if (this.getAttribute("data-type") === "rock") {
-
-                checkWinner()
+                userChoice = "rock"
+                console.log(userChoice);
+                checkWinner();
             } else if (this.getAttribute("data-type") === "paper") {
-
-                checkWinner()
+                userChoice = "paper"
+                console.log(userChoice);
+                checkWinner();
             } else if (this.getAttribute("data-type") === "scissors") {
-
-                checkWinner()
+                userChoice = "scissors"
+                console.log(userChoice);
+                checkWinner();
             } else if (this.getAttribute("data-type") === "stop") {
-
                 finish()
-            }
+            } 
+           
         })
     }
+
 })
 
-
-function computerChooses() {
-    let rpsChoices = ['Rock', 'Paper', 'Scissors']
+function computerChoice() {
+    let rpsChoices = ["rock", "paper", "scissors"]
     let computerChoice = rpsChoices[Math.floor(Math.random() * 3)]
     return computerChoice
+}
+
+
+
+
+// Increment functions borrowed from the walkthrough project.
+function incrementUserScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+    gameCount()
+}
+
+// Increment functions borrowed from the walkthrough project.
+function incrementComputerScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+    gameCount()
 }
