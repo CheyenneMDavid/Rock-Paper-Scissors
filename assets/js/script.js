95afaad1ea8861029aed7357acda2dcac79eb202
CheyenneMDavid <Cheyennedavid68@gmail.com>
2022-12-21 22:17:08 +0000

    Add function to limit gameplay to five games

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
-    let userChoice = ""
+for (let button of buttons) {
+    button.addEventListener("click", function () {
 
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
+        if (this.getAttribute("data-type") === "rock") {
+            userChoice = "rock"
+            console.log(userChoice);
+            checkWinner();
+        } else if (this.getAttribute("data-type") === "paper") {
+            userChoice = "paper"
+            console.log(userChoice);
+            checkWinner();
+        } else if (this.getAttribute("data-type") === "scissors") {
+            userChoice = "scissors"
+            console.log(userChoice);
+            checkWinner();
+        } else if (this.getAttribute("data-type") === "stop") {
+            finish()
+        }
 
-            if (this.getAttribute("data-type") === "rock") {
-                userChoice = "rock"
-                console.log(userChoice);
-                checkWinner();
-            } else if (this.getAttribute("data-type") === "paper") {
-                userChoice = "paper"
-                console.log(userChoice);
-                checkWinner();
-            } else if (this.getAttribute("data-type") === "scissors") {
-                userChoice = "scissors"
-                console.log(userChoice);
-                checkWinner();
-            } else if (this.getAttribute("data-type") === "stop") {
-                finish()
-            } 
-           
-        })
-    }
+    })
+}
 
 })
 
@@ -36,8 +34,27 @@ function computerChoice() {
     return computerChoice
 }
 
+function checkWinner() {
+    if (userChoice === computerChoice) {
+        alert("This game was a draw");
 
 
+    } else if (userChoice === "rock" && computerChoice === "scissors") {
+        alert("Welldone! You chose Rock and the Computer chose Scissors, which mans YOU WIN!");
+        incrementUserScore();
+
+    } else if (userChoice === "paper" && computerChoice === "rock") {
+        alert("Welldone! You chose Paper and the Computer chose Rock, which mans YOU WIN!");
+        incrementUserScore();
+
+    } else if (userChoice === "scissors" && computerChoice === "paper") {
+        alert("Welldone! You chose Scissors and the Computer chose Paper, which mans YOU WIN!");
+        incrementUserScore();
+
+    } else
+        alert(`Unlucky!  You chose "${userChoice}" and the computer choice "${computerChoice}", which means you loose!`);
+    incrementComputerScore()
+}
 
 // Increment functions borrowed from the walkthrough project.
 function incrementUserScore() {
@@ -53,3 +70,13 @@ function incrementComputerScore() {
     document.getElementById("score").innerText = ++oldScore;
     gameCount()
 }
+
+
+// Loop to play five games before stopping
+function gameCount() {
+
+    for (let i = 0; i < 5; i++) {
+        playGame()
+    }
+}
+gameCount()
