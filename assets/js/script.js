a74d925358bc70daf5a534f323c3d76eb785b45e
CheyenneMDavid <Cheyennedavid68@gmail.com>
2022-12-22 15:47:10 +0000

    Edit checkWinner function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
-
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
-
 for (let button of buttons) {
     button.addEventListener("click", function () {
-
         if (this.getAttribute("data-type") === "rock") {
             userChoice = "rock"
             console.log(userChoice);
@@ -22,40 +19,30 @@ for (let button of buttons) {
         } else if (this.getAttribute("data-type") === "stop") {
             finish()
         }
-
     })
 }
-
 })
-
 function computerChoice() {
     let rpsChoices = ["rock", "paper", "scissors"]
     let computerChoice = rpsChoices[Math.floor(Math.random() * 3)]
     return computerChoice
 }
-
 function checkWinner() {
     if (userChoice === computerChoice) {
         alert("This game was a draw");
-
-
     } else if (userChoice === "rock" && computerChoice === "scissors") {
-        alert("Welldone! You chose Rock and the Computer chose Scissors, which mans YOU WIN!");
+        alert("You win. You chose Rock and the Computer chose Scissors.");
         incrementUserScore();
-
     } else if (userChoice === "paper" && computerChoice === "rock") {
-        alert("Welldone! You chose Paper and the Computer chose Rock, which mans YOU WIN!");
+        alert("You win. You chose Paper and the Computer chose Rock.");
         incrementUserScore();
-
     } else if (userChoice === "scissors" && computerChoice === "paper") {
-        alert("Welldone! You chose Scissors and the Computer chose Paper, which mans YOU WIN!");
+        alert("You win. You chose Scissors and the Computer chose Paper.");
         incrementUserScore();
-
     } else
-        alert(`Unlucky!  You chose "${userChoice}" and the computer choice "${computerChoice}", which means you loose!`);
+        alert(`You lose. You chose "${userChoice}" and the computer chose "${computerChoice}"`);
     incrementComputerScore()
 }
-
 // Increment functions borrowed from the walkthrough project.
 function incrementUserScore() {
     let oldScore = parseInt(document.getElementById("score").innerText);
@@ -74,6 +61,16 @@ function incrementComputerScore() {
 function gameCount() {
 
     for (let i = 0; i < 5; i++) {
+
+    
+          
+            
+    
+
+          
+    
+    
+  
         playGame()
     }
 }
